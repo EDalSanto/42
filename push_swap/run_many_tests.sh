88e4d04568cbe01e2424ed1fad488183ca4ff9aa
Eddie <ejds001@gmail.com>
2016-12-09 21:23:47 -0800

    shell script test

--- push_swap/run_many_tests.sh
+++ push_swap/run_many_tests.sh
@@ -1,9 +1,36 @@
-for i in {1..10}
+#!/bin/bash
+TOTAL=0
+BOLD=$(tput bold)
+LIGHT_GREEN='\033[1;32m'
+PURPLE='\033[0;35m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+echo "25 Tests Commencing.."
+for i in {1..25}
 do
 	ARG=$(sh GenRandNums.sh)
-	echo "Random numbers stored in ARG for Trial $i"
-	echo "Number of operations needed: "
-	./push_swap $ARG | wc -l
-	echo "Checker: "
-	./push_swap $ARG | ./checker $ARG
+	echo "${PURPLE}Trial: $i${NC}"
+	printf "Number of operations needed: " 
+	NUM=$(./push_swap $ARG | wc -l)
+	if [[ "$NUM" -gt 5300 ]] 
+	then
+		echo "${RED}Your program took $NUM operations; The limit is 5300!${NC}"
+		continue
+	fi	
+	TOTAL=$((TOTAL+$NUM))
+	echo "${BOLD}${BLUE}$NUM${NC}"
+	printf "Checker: "
+	RES=$(./push_swap $ARG | ./checker $ARG)
+	if [ "$RES" == "OK" ]
+	then
+		echo ${LIGHT_GREEN}"SUCCESS for Trial $i!${NC}"
+	elif [ "$RES" == "KO" ]
+	then
+		echo ${RED}"FAILURE for Trial $i!${NC}"
+	fi
+	echo "------------------------------"
 done
+echo "Avg operations: ${PURPLE}$((TOTAL/25))${NC}"
